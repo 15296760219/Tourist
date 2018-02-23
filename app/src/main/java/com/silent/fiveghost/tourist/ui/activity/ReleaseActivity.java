0097063d24ff7a31a14ea99b4377790d3aa70789
z651126 <1318473214@qq.com>
2018-02-23 20:53:49 +0800

    路线详情，订单填写界面，发布行程

--- app/src/main/java/com/silent/fiveghost/tourist/ui/activity/ReleaseActivity.java
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/ReleaseActivity.java
@@ -4,19 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.silent.fiveghost.tourist.R;
 import com.silent.fiveghost.tourist.app.BaseActivity;
 
-/**
- * 订单内容  填写版
- */
-public class OrderContentActivity extends BaseActivity implements View.OnClickListener {
-
-
-    private Button mSubmit_order_content;
-    private ImageView mReturn_order_content;
+public class ReleaseActivity extends BaseActivity implements View.OnClickListener {
+    private Button btn_release;
 
     @Override
     protected void initLayout() {
@@ -27,7 +20,7 @@ public class OrderContentActivity extends BaseActivity implements View.OnClickLi
         //是否允许全屏
         setAllowFullScreen(true);
         //设置布局
-        setContentView(R.layout.activity_order_content);
+        setContentView(R.layout.release_itinerary);
         //是否是沉浸式状态栏
         setSteepStatusBar(true);
     }
@@ -39,19 +32,15 @@ public class OrderContentActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     protected void initView() {
-        mReturn_order_content = findViewById(R.id.return_order_content);
-        mReturn_order_content.setOnClickListener(this);
-        mSubmit_order_content = findViewById(R.id.submit_order_content);
-        mSubmit_order_content.setOnClickListener(this);
+        btn_release = findViewById(R.id.btn_release);
+        btn_release.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.return_order_content:
-                finish();
-                break;
-            case R.id.submit_order_content:
+            case R.id.btn_release:
+//                startActivity();
                 break;
         }
     }
