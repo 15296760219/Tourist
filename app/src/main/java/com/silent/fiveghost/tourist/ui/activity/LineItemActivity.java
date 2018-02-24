8fe66a70036af7f0b20e166eb4a4031f0af7fa61
liudou123 <1225219952@qq.com>
2018-02-24 11:54:45 +0800

    yi订单

---
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/LineItemActivity.java
@@ -0,0 +1,15 @@
+package com.silent.fiveghost.tourist.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.silent.fiveghost.tourist.R;
+
+public class LineItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_order);
+    }
+}
---
+++ app/src/main/java/com/silent/fiveghost/tourist/ui/activity/OrderFormActivity.java
@@ -0,0 +1,14 @@
+package com.silent.fiveghost.tourist.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.silent.fiveghost.tourist.R;
+
+public class OrderFormActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.published_order);
+    }
+}
